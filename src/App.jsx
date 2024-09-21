5066ae1cfcb8d6a701ebc0153bf6881b19b39ec6
Milos Njegovanovic <47067010+MilosNj@users.noreply.github.com>
2024-09-21 18:32:59 +0200

    Added a Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Box, useColorModeValue } from '@chakra-ui/react'
 
+import Header from './components/Header'
+
 const App = () => (
-  <Box minHeight={'100vh'} bg={useColorModeValue('gray.100', 'gray.900')}>
-    {/* <Navbar /> */}
+  <Box minHeight='100vh' bg={useColorModeValue('gray.100', 'gray.900')}>
+    <Header />
   </Box>
 )
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Container,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md'
+
+const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <Container maxW='1140px' px={4}>
+      <Flex
+        h={16}
+        alignItems='center'
+        justifyContent='space-between'
+        flexDir={{ base: 'column', sm: 'row' }}
+      >
+        <Text
+          fontSize={{ base: '22', sm: '28' }}
+          fontWeight='bold'
+          textTransform='uppercase'
+          textAlign='center'
+          bgGradient='linear(to-r, green.400, blue.500)'
+          bgClip='text'
+        >
+          Movie Grid 📽️
+        </Text>
+        <Button onClick={toggleColorMode} bg={useColorModeValue('gray.300')}>
+          {colorMode === 'light' ? (
+            <MdDarkMode fontSize={30} />
+          ) : (
+            <MdOutlineLightMode fontSize={30} />
+          )}
+        </Button>
+      </Flex>
+    </Container>
+  )
+}
+
+export default Header
