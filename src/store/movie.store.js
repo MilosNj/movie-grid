12a50f8c2a40ff03b412762684f6f280cb8f12bd
Milos Njegovanovic <47067010+MilosNj@users.noreply.github.com>
2024-09-21 20:09:31 +0200

    Changed the movie store logic

--- src/store/movie.store.js
+++ src/store/movie.store.js
@@ -2,38 +2,36 @@ import { create } from 'zustand'
 
 export const useMovieStore = create((set) => ({
   movies: [],
-  setMovies: (movies) => {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || {}
-
-    const uniqueMovies = Array.from(
-      new Map(
-        movies.map((movie) => {
-          const uniqueKey = movie.original_title || movie.title
-          return [uniqueKey, movie]
-        })
-      ).values()
-    )
-
-    const sortedMovies = uniqueMovies.sort((a, b) => {
-      const imdbRatingA =
-        a.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
-      const imdbRatingB =
-        b.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
-      return imdbRatingB - imdbRatingA
-    })
-
-    const moviesWithFavorites = sortedMovies.map((movie) => ({
-      ...movie,
-      isFavorite: !!favorites[movie.id]
-    }))
-
-    set({ movies: moviesWithFavorites })
-  },
+  setMovies: (movies) => set({ movies }),
   fetchMovies: async () => {
     try {
       const res = await fetch('../assets/movies.json')
       const data = await res.json()
-      set({ movies: data })
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || {}
+
+      const uniqueMovies = Array.from(
+        new Map(
+          data.map((movie) => {
+            const uniqueKey = movie.original_title || movie.title
+            return [uniqueKey, movie]
+          })
+        ).values()
+      )
+
+      const sortedMovies = uniqueMovies.sort((a, b) => {
+        const imdbRatingA =
+          a.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
+        const imdbRatingB =
+          b.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
+        return imdbRatingB - imdbRatingA
+      })
+
+      const moviesWithFavorites = sortedMovies.map((movie) => ({
+        ...movie,
+        isFavorite: !!favorites[movie.id]
+      }))
+
+      set({ movies: moviesWithFavorites })
     } catch (error) {
       console.error('Error fetching the movie data:', error)
     }
