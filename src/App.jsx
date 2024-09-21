e59c3a1207b360c8ec251fd61002260e8ac75d9a
Milos Njegovanovic <47067010+MilosNj@users.noreply.github.com>
2024-09-21 21:09:01 +0200

    displaying the empty home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Box, useColorModeValue } from '@chakra-ui/react'
 
 import Header from './components/Header'
+import HomePage from './pages/HomePage'
 
 const App = () => (
   <Box minHeight='100vh' bg={useColorModeValue('gray.100', 'gray.900')}>
     <Header />
+    <HomePage />
   </Box>
 )
 
