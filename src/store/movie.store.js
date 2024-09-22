ff1578f6748de43f1c2c026764593c7310996a38
Milos Njegovanovic <47067010+MilosNj@users.noreply.github.com>
2024-09-22 11:57:43 +0200

    implemented the persist middleware to handle localStorage better, used the partialize option to only store the favorites in localStorage

--- src/store/movie.store.js
+++ src/store/movie.store.js
@@ -1,64 +1,78 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useMovieStore = create((set) => ({
-  movies: [],
-  setMovies: (movies) => set({ movies }),
-  fetchMovies: async () => {
-    try {
-      const res = await fetch('/movies.json')
-      const data = await res.json()
-      const favorites = JSON.parse(localStorage.getItem('favorites')) || {}
+export const useMovieStore = create(
+  persist(
+    (set, get) => ({
+      movies: [],
+      setMovies: (movies) => set({ movies }),
+      fetchMovies: async () => {
+        try {
+          const res = await fetch('/movies.json')
+          const data = await res.json()
+          const favorites = get().favorites || {}
 
-      const uniqueMovies = Array.from(
-        new Map(
-          data.map((movie) => {
-            const uniqueKey = movie.original_title || movie.title
-            return [uniqueKey, movie]
+          const uniqueMovies = Array.from(
+            new Map(
+              data.map((movie) => {
+                const uniqueKey = movie.original_title || movie.title
+                return [uniqueKey, movie]
+              })
+            ).values()
+          )
+
+          const sortedMovies = uniqueMovies.sort((a, b) => {
+            const imdbRatingA =
+              a.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
+            const imdbRatingB =
+              b.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
+            return imdbRatingB - imdbRatingA
           })
-        ).values()
-      )
 
-      const sortedMovies = uniqueMovies.sort((a, b) => {
-        const imdbRatingA =
-          a.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
-        const imdbRatingB =
-          b.ratings.find((rating) => rating.id === 'imdb')?.rating || 0
-        return imdbRatingB - imdbRatingA
-      })
+          const moviesWithFavorites = sortedMovies.map((movie) => ({
+            ...movie,
+            isFavorite: !!favorites[movie.id]
+          }))
 
-      const moviesWithFavorites = sortedMovies.map((movie) => ({
-        ...movie,
-        isFavorite: !!favorites[movie.id]
-      }))
+          set({ movies: moviesWithFavorites })
+        } catch (error) {
+          console.error('Error fetching movies:', error)
+          return { success: false, message: error.message }
+        }
+      },
+      favorites: {},
+      toggleFavorite: (id) => {
+        try {
+          set((state) => {
+            const updatedMovies = state.movies.map((movie) =>
+              movie.id === id
+                ? { ...movie, isFavorite: !movie.isFavorite }
+                : movie
+            )
 
-      set({ movies: moviesWithFavorites })
-    } catch (error) {
-      return { success: false, message: error }
+            const updatedFavorites = { ...state.favorites }
+            if (updatedFavorites[id]) {
+              delete updatedFavorites[id]
+            } else {
+              updatedFavorites[id] = true
+            }
+
+            return {
+              movies: updatedMovies,
+              favorites: updatedFavorites
+            }
+          })
+
+          return { success: true }
+        } catch (error) {
+          console.error('Error toggling favorite:', error)
+          return { success: false, message: error.message }
+        }
+      }
+    }),
+    {
+      name: 'movie-storage',
+      partialize: (state) => ({ favorites: state.favorites })
     }
-  },
-  toggleFavorite: (id) => {
-    try {
-      set((state) => {
-        const updatedMovies = state.movies.map((movie) =>
-          movie.id === id ? { ...movie, isFavorite: !movie.isFavorite } : movie
-        )
-
-        const favorites = JSON.parse(localStorage.getItem('favorites')) || {}
-        updatedMovies.forEach((movie) => {
-          if (movie.isFavorite) {
-            favorites[movie.id] = true
-          } else {
-            delete favorites[movie.id]
-          }
-        })
-        localStorage.setItem('favorites', JSON.stringify(favorites))
-
-        return { movies: updatedMovies }
-      })
-
-      return { success: true }
-    } catch (error) {
-      return { success: false, message: error }
-    }
-  }
-}))
+  )
+)
